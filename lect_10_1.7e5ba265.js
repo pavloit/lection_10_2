2db5cb10444a6f95db717b85f49de82a6f2ac7b6
pavloit <135540979+pavloit@users.noreply.github.com>
2023-11-20 10:54:40 +0000

    Deploying to gh-pages from @ pavloit/lection_10_2@6400956915a7d07d9bf00df7efb5f61c28da8669 🚀

--- lect_10_1.7e5ba265.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),r=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){e.setAttribute("disabled","true"),r.removeAttribute("disabled"),d=setInterval(o,1e3)})),r.addEventListener("click",(function(){e.removeAttribute("disabled"),r.setAttribute("disabled","true"),clearInterval(d)}));let d=1;function o(){t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}r.setAttribute("disabled","true");
-//# sourceMappingURL=lect_10_1.7e5ba265.js.map
